24429ea9b623fd4bde9bf30907f5828334f787af
webdev-710 <54043964+webdev-710@users.noreply.github.com>
2020-01-08 13:05:20 -0500

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,12 +34,17 @@ class AuthController extends Controller
   }
 
   public function register(Request $request){
-      return "a";
-      $this->validate($request, [
+      $validator = Validator::make($request->all(), [
         'name' => 'required|string|max:255',
         'email' => 'required|string|email|max:255|unique:users',
         'password' => 'required|string',
-      ]);
+    ]);
+    if($validator->fails()) {
+        return response()->json([
+            'result' => "error",
+            'errors' => $validator->errors()->first()
+        ], 400);
+    }
       
       $user = User::create([
         'name' => $request->name,
